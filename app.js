4adde8831a23ebb50c54e993623da1f27480d914
Ameen Shah <Ameen Shah>
2016-10-30 00:52:14 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,368 @@
+// var spec = prompt("Your Species");
+// alert(spec);
+
+
+
+// var name = prompt("What is your Name : ");
+// alert(name + " Shah");
+
+
+
+// var name1 = prompt("Name", "What is your Name : ");
+// alert(name1 + "Amin Shah");
+
+
+
+// var val1 = prompt("Enter a First Number");
+// var val2 = prompt("Enter a Second Number");
+// alert (val1 + val2);
+
+
+
+
+// var val1 = prompt("Enter a First value");
+// var val2 = prompt("Enter a Second value");
+// alert(parseInt(val1) + parseInt(val2));
+
+
+
+//MarkSheet
+// var mark = prompt("Enter Your Marks");
+// if (mark == 90) {
+//     alert("A+ Grade");
+// } 
+// if (mark == 80) {
+//     alert("A1 Grade");
+// }
+// if (mark == 70) {
+//     alert("A Grade");
+// }
+// if (mark == 60) {
+//     alert("B Grade");
+// }
+// if (mark == 50) {
+//     alert("C Grade");
+// }
+// if (mark == 40) {
+//     alert("D Grade");
+// }
+
+
+
+
+// var mark1 = prompt("Enter Your Percentage");
+
+// if (mark1 == 20) {
+//     alert("Percetage" +"  "+ 20/20*100 +'%,' +  "         "  + "Grade A+");
+// }
+// if (mark1 == 19) {
+//     alert("Percetage" +"  "+ 19/20*100 + '%,' + "        "  + "Grade A+");
+// } 
+// if (mark1 == 18) {
+//     alert("Percetage" +"  "+ 18/20*100 + '%,' + "        "  + "Grade A1");
+// } 
+// if (mark1 == 17) {
+//     alert("Percetage" +"  "+ 17/20*100 + '%,' + "        "  + "Grade A1");
+// } 
+// if (mark1 == 16) {
+//     alert("Percetage" +"  "+ 16/20*100 + '%,' + "        "  + "Grade A");
+// } 
+// if (mark1 == 15) {
+//     alert("Percetage" +"  "+ 15/20*100 + '%,' + "        "  + "Grade A");
+// } 
+// if (mark1 == 14) {
+//     alert("Percetage" +"  "+ 14/20*100 + '%,' + "        "  + "Grade B");
+// } 
+// if (mark1 == 13) {
+//     alert("Percetage" +"  "+ 13/20*100 + '%,' + "        "  + "Grade B");
+// } 
+// if (mark1 == 12) {
+//     alert("Percetage" +"  "+ 12/20*100 + '%,' + "        "  + "Grade C");
+// } 
+// if (mark1 == 11) {
+//     alert("Percetage" +"  "+ 11/20*100 + '%,' + "        "  + "Grade C");
+// } 
+// if (mark1 == 10) {
+//     alert("Percetage" +"  "+ 10/20*100 + '%,' + "        "  + "Grade D");
+// } 
+// if (mark1 == 9) {
+//     alert("Percetage" +"  "+ 9/20*100 + '%,' + "        "  + "Grade D");
+// } 
+// if (mark1 == 8) {
+//     alert("Percetage" +"  "+ 8/20*100 + '%,' + "        "  + "Grade E");
+// } 
+// if (mark1 == 7) {
+//     alert("Percetage" +"  "+ 7/20*100 + '%,' + "        "  + "Grade E");
+// } 
+// if (mark1 == 6) {
+//     alert("Percetage" +"  "+ 6/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 5) {
+//     alert("Percetage" +"  "+ 5/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 4) {
+//     alert("Percetage" +"  "+ 4/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 3) {
+//     alert("Percetage" +"  "+ 3/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 2) {
+//     alert("Percetage" +"  "+ 2/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 1) {
+//     alert("Percetage" +"  "+ 1/20*100 + '%,' + "        "  + "Grade -");
+// } 
+
+
+
+
+// var Id = prompt("Enter Your ID");
+// var Name = prompt("Enter Your Name");
+
+// if (Id == 1 && Name == 'Ehsan') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 20 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ +20/20*100 +'%\n' + "Grade A+");
+// }
+// if (Id == 2 && Name == 'Sami') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 19 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 19/20*100 +'%\n' + "Grade A+");
+// }
+// if (Id == 3 && Name == 'Amin') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 18 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 18/20*100 +'%\n' + "Grade A");
+// }
+// if (Id == 4 && Name == 'Faisal') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 17 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 17/20*100 +'%\n' + "Grade A");
+// }
+// if (Id == 5 && Name == 'Saad') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 16 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 16/20*100 +'%\n' + "Grade B");
+// }
+// if (Id == 6 && Name == 'Ahmed') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 15 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 15/20*100 +'%\n' + "Grade B");
+// }
+// if (Id == 7 && Name == 'Hameed') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 14 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 14/20*100 +'%\n' + "Grade C");
+// }
+// if (Id == 8 && Name == 'Ali') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 13 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ +13/20*100 +'%\n' + "Grade C");
+// }
+// if (Id == 9 && Name == 'Hasan') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 12 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ +12/20*100 +'%\n' + "Grade D");
+// }
+// if (Id == 10 && Name == 'Waqas') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 11 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 11/20*100 +'%\n' + "Grade D");
+// }
+// if (Id == 11 && Name == 'Bilal') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 10 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 10/20*100 +'%\n' + "Grade E");
+// }
+// if (Id == 12 && Name == 'Ahsan') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 9 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 9/20*100 +'%\n' + "Grade E");
+// }
+// if (Id == 13 && Name == 'Noman') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 8 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 8/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 14 && Name == 'Amir') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 7 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 7/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 15 && Name == 'Ejaz') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 6 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 6/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 16 && Name == 'Khalil') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 5 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 5/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 17 && Name == 'Husnain') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 4 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 4/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 18 && Name == 'Khuzair') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 3 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 3/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 19 && Name == 'Mahmood') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 2 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 2/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 19 && Name == 'Mohsin') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 1 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 1/20*100 +'%\n' + "Grade -");
+// }
+
+
+
+// var names = []
+// names[0] = "Ahmed"
+// names[1] = "Amin"
+// names[2] = "Ehsan"
+// names.push("Faisal", "Khalil");
+// alert(names)
+
+// var now = new Date();
+// var thHour = now.getHours;
+// var thMinute = now.getMinutes;
+// var thSecond = now.getSeconds;
+
+// alert("Hour" + thHour + "Minute" + thMinute + "Second" + thSecond  );
+
+
+// var a = 15;
+// var a = 30;
+// alert(a);
+
+
+// var a = prompt ("Enter a number");
+// if(a === "1"){
+//     userIQ = "genius";
+//     alert("Welcome " + userIQ);
+// }else {
+//  userIQ = "problematic";
+//  alert("Incorrect " + userIQ);
+//  }
+
+// var weight = prompt("Enter weight");
+// var time = prompt("Enter time");
+// var age = prompt("Enter age");
+// var gender = prompt("Enter gender");
+
+// if (weight > 300 && time < 6 || age > 17 && gender === "male") {
+//     alert("Come to our tryout!");
+// } else {
+//     alert("Come to our cookout!");
+//  }
+
+
+// var age = prompt("Enter Your Age");
+// var res = prompt("Enter Your Nationality");
+// if (age > 65 || age < 21 && res === "US") {
+//     alert("Wellcome");
+// }
+
+
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+// cities.pop();
+// cities.push("karachi", "Hyderabad");
+// cities.shift();
+// cities.unshift("karachi", "Hyderabad");
+// cities.splice(2,2,"karachi", "Hyderabad");
+// cities.splice(2,0,"karachi", "Hyderabad");
+// cities.splice(0,2,"karachi", "Hyderabad");
+// alert("Welcome to " + cities);
+
+// var CapitalCities = cities.slice(0,4);
+// alert(CapitalCities);
+
+
+// var cityToCheck = prompt("Enter Your CityName");
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"];
+// for (var i = 0; i <= 4; i++) {
+//     if (cityToCheck === cities[i]) {
+//     alert( cities[i++] + "It's one of the cleanest cities");
+//     }
+// }
+
+
+// for (var i = 0; i <= 4; i+=2) {
+//     alert("Hello" + i);
+// }
+
+// for (var i = 0; i <= 4; i= i+1) {
+//     alert("Hello" + i);
+// }
+
+
+
+
+// var names = ["Milk ", "Sugar ", "Flour "];
+// var names1 = ["BetelNut ", "Milk "];
+// var names2 = ["Sugar ", "Milk "];
+// var uniqueNames = [];
+
+
+// function myFunc(){
+//   for(var i in names, names1, names2){
+//     if(uniqueNames.indexOf(names[i]) + (names1[i]) + (names2[i]) === -1){
+//          uniqueNames.push(names[i]);
+//          uniqueNames.push(names1[i]);
+//          uniqueNames.push(names2[i]);
+//      }
+//   }
+//     alert(uniqueNames);
+// }
+
+// myFunc();    
+
+
+// var names = ["Alex","Tony","James","Suzane", "Marie", "Laurence", "Alex", "Suzane", "Marie", "Marie", "James", "Tony", "Alex"];
+// var names1 = ["Faisal","Khalid","James","Suzane", "Marie", "Laurence", "Alex", "Suzane", "Marie"];
+// var names2 = ["shahid","khan","James","Suzane", "Marie", "Laurence", "Alex", "Suzane", "Marie", "Marie"];
+
+
+
+// var array1 = new Array("a","b","c","d","e","f");
+// var array2 = new Array("c","e", "k");
+
+// for (var i = 0; i<array2.length; i++) {
+//     var arrlen = array1.length;
+//     for (var j = 0; j<arrlen; j++) {
+//         if (array2[i] == array1[j]) {
+//             array = array1.slice(0, j).concat(array1.slice(j, array2));
+//         }
+//     }
+// }
+// alert(array);
+
+
+
+// var array1 = ["Vijendra","Singh"];
+// var array2 = ["Singh", "Shakya"];
+// var array3 = array1.concat(array2); 
+
+// alert(array3);
+
+
+// function arrayUnique(array) {
+//     var a = array.concat();
+//     for(var i=0; i<a.length; ++i) {
+//         for(var j=i+1; j<a.length; ++j) {
+//             if(a[i] === a[j])
+//                 a.splice(j--, 1);
+//         }
+//     }
+
+//     return a;
+// }
+
+// var recipe1 = ["Sugar", "Milk", "Flour"];
+// var recipe2 = ["Egg", "SunFlower Oil" ,"Sugar", "Milk", "Honey"];
+// var recipe3 = ["Apple Syrup", "Egg", "Sugar", "Flour", "Berries", "Milk"]
+// var recipe4 = [];
+
+// recipe4 = arrayUnique(recipe1.concat(recipe2));
+// recipe4 = arrayUnique(recipe4.concat(recipe3));
+// alert(recipe4);
+
+
+function generateTable()
+{
+     var myVar = prompt("A number?", "");
+     var myString = "";
+     for (i=1; i<=20; i++) {
+       myString += myVar+ " x " + i + " = " + (i*myVar) + "\n";
+     }
+     alert(myString);
+}
+generateTable();
+
+
+
+// function arrayUnique(array) {
+//     var a = array.concat();
+//     for(var i=0; i<a.length; ++i) {
+//         for(var j=i+1; j<a.length; ++j) {
+//             if(a[i] === a[j])
+//                 a.splice(j--, 1);
+//         }
+//     }
+
+//     return a;
+// }
+
+// var recipe1 = ["Sugar", "Milk", "Flour"];
+// var recipe2 = ["Egg", "SunFlower Oil" ,"Sugar", "Milk", "Honey"];
+// var recipe3 = ["Apple Syrup", "Egg", "Sugar", "Flour", "Berries"]
+
+// var recipe4 = arrayUnique(recipe1.concat(recipe2));
+// var recipe5 = arrayUnique(recipe4.concat(recipe3));
+// alert(recipe5);
---
+++ app1.js
@@ -0,0 +1,159 @@
+
+/*Chapter # 01*/
+// alert("Heloo");
+// alert("  Heloo  ");
+// alert(  "Heloo" );
+// alert('Heloo');
+
+
+/*Chapter # 02*/
+// var myName = "Amin";
+// myName = "Amin Shah";
+// alert(myName);
+
+
+/*Chapter # 03*/
+// var abc = 150;
+// abc = 25;
+// alert(abc);
+
+// var a = 150;
+// a = 150 + 10;
+// alert(a);
+
+// var a = 150;
+// a = a + 10;
+// alert(a);
+
+// var a = 150;
+// var b = a + 10;
+// alert(b);
+
+// var a = 150;
+// var b = 160;
+// alert(a + b);
+
+// alert(150);
+// alert(@);
+// alert(123am234in);
+// alert(123#);
+// alert(12 434);
+// alert();
+
+
+/*Chapter # 04*/
+// var 1st = "amin";
+// alert(1st);
+
+// var my Var = "ahmed";
+// alert(my Var);
+
+// var myVar = "ahmed";
+// alert(myVar);
+
+// var myVar1 = "faisal";
+// alert(myVar1);
+
+// var my1Var = "sami";
+// alert(my1Var);
+
+// var _myVar = "shah";
+// alert(_myVar);
+
+// var $myVar = "tahil";
+// alert($myVar);
+
+// var #myVar = "shah";
+// alert(#myVar);
+
+// var break = "ehsan";
+// alert(break);
+
+
+/*Chapter # 05*/
+// var a = 2+1;
+// alert(a);
+
+// alert(2+7);
+
+// alert(2*9);
+
+// var a = 150;
+// var b = 160;
+// alert(a + b);
+
+// var a = 9;
+// var b = 3;
+// alert(a%b);
+
+// var a = 10;
+// var b = 3;
+// alert(a%b);
+
+
+
+/*Chapter # 10*/
+// var x = prompt("Enter a Pass");
+// if(x === "Amin")
+// {
+//     alert("Wellcome");
+// }
+
+
+/*Chapter # 12*/
+// var x = prompt("Enter a Pass");
+// var pass = "Amin";
+// if (x === pass) {
+//     alert("Welcome");
+// }else {
+//     alert("Wrong Answer");
+// }
+
+
+// var x = prompt("Enter a City");
+// var pass = "Mardan";
+// if (x === pass) {
+//     alert("Correct!");
+// }else if (x === "Peshawar") {
+//     alert("Incorrect but close!");
+// }else {
+//     alert("Wrong Answer");
+// }
+
+
+// var age = prompt("Enter a age");
+// var age = prompt("Enter a City");
+// var res = prompt("Enter a City");
+
+// if ((age > 65 || age < 21) && res === "US") { 
+//     alert("Hello");
+// }
+
+
+// var pets = ["dog", "cat", "fly", "bug", "ox"];
+// pets.splice(2, 2); 
+// alert(pets);
+
+
+// var str = prompt("Enter some text");
+// var numChars = str.length;
+// for (var i = 0; i < numChars; i++) {
+//     if (str.slice(i, i+2) === " ") {
+//         alert("No double spaces!");
+//         break;
+//     }
+// }
+
+// var myDate = new Date();
+// var myDate1 = myDate.getDate();
+// alert(myDate1);
+
+
+//  function tellTime() {
+//  var now = new Date();
+//  var theHr = now.getHours();
+//  var theMin = now.getMinutes();
+//  alert("Current time: "+ theHr + ":" + theMin);
+//  }
+
+//  tellTime();
